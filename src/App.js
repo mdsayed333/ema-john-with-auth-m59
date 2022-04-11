740f6cee7debed04334b83fff0983c31359fc992
Sayed <mdsayedgv@gmail.com>
2022-04-11 15:17:19 +0600

    inplemented login and redirectto shop

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
+import NotFound from './components/NotFound/NotFound';
 import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -13,10 +16,14 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Shop></Shop>}></Route>
+        <Route path='/home' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='*'element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
